87cdc8cb0b0ac150fe7cf49b4cb752c615e8c0dc
Edyta Bednarz <edy.b.bednarz@gmail.com>
2019-01-16 16:38:35 +0000

    Basic test for article model + config.

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe '#validations' do
+
+    it 'should test that the factory is valid' do
+      expect(build :article).to be_valid
+    end
+    
+    it 'should validate the presence of the title' do
+      article = build :article, title: ''
+      expect(article).not_to be_valid
+      expect(article.errors.messages[:title]).to include("can't be blank")
+    end
+
+  end
+
 end
